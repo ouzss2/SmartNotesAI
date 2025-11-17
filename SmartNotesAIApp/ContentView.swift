13b94afe94e3b91b23a231d09799f1eb02c40136
nihel <slitinihel023@gmail.com>
2025-11-17 13:17:15 +0100

    Initial Commit

---
+++ SmartNotesAIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartNotesAIApp
+//
+//  Created by Tekup-mac-1 on 17/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartNotesAIApp/SmartNotesAIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartNotesAIAppApp.swift
+//  SmartNotesAIApp
+//
+//  Created by Tekup-mac-1 on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartNotesAIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
